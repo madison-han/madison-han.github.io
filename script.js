efe4e6b6f53951468a9387207a658c80377937b2
madison-han <104019938+madison-han@users.noreply.github.com>
2023-07-01 23:37:56 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+about();
+
+ window.onscroll = function () {
+   if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
+     document.getElementById("header").style.fontSize = "1vw";
+  } else {
+     document.getElementById("header").style.fontSize = "1.5vw";
+   }
+ };
+
+function about() {
+  document.getElementById('txt').innerHTML = "<div class='p'><h2>Hello!</h2><hr>" +
+    "<br>My name is <b>Madison Han</b>." +
+    "<br><br> I'm a second year at the University of Waterloo double majoring in <b>Computational Mathematics</b> and <b>Statistics</b>.<br>"+
+    "<br> In my freetime, I enjoy programming, rock climbing, hiking, board games, and Star Trek!<br><br> </div>" +
+    "<br>";
+}
+
+function projects() {
+  document.getElementById('txt').innerHTML = "<div class='p'><h2><a href='https://github.com/madison-han/Weather-App'>WeatherApp</a></h2><hr>"+
+    "<br>This <b>Java</b> application allows users to retrieve weather information for a specific city using the OpenWeatherMapp <b>API</b>.<br></div>"+
+    "<div class='p'><h2><a href='https://github.com/madison-han/To-Do-List'>ToDo</a></h2><hr>"+
+    "<br>A simple to-do list application built with React! Tasks can be added, marked as complete, and deleted.</div>"+
+    "<div class='p'><h2><a href='https://github.com/madison-han/Scripts-for-CS-246'>Bash Scripts</a></h2><hr>"+
+    "<br>Various Bash scripts written for testing automation in an Object Oriented Programming course.<br></div>"+
+    "<div class='p'><h2><a href='https://github.com/madison-han/madison-han.github.io'>This Site!</a></h2><hr>"+
+    "<br>Created with HTML, CSS, and Javascript.<br></div>" +
+    "<br>";
+}
+
+function contact() {
+  document.getElementById('txt').innerHTML = "<div class='p'><h2>Contact Me!</h2><hr>" +
+  "<br>Phone Number: 437-262-5468" +
+  "<br>Email: <a href='mailto:madisonhan04@gmail.com'>madisonhan04@gmail.com</a>" +
+  "<br>Github: <a href='https://github.com/madison-han'>madison-han</a>" +
+  "<br>LinkedIn: <a href='https://www.linkedin.com/in/madison-han-01b5701a4/'>Madison Han</a><br>" +
+  "</div><br>"; 
+}
